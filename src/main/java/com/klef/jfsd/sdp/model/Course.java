0fa273916330888d6f67044f4ac39163f13254f8
klu-2100030455 <2100030455@kluniversity.in>
2023-10-14 17:15:14 +0530

    Modified course.java

--- src/main/java/com/klef/jfsd/sdp/model/Course.java
+++ src/main/java/com/klef/jfsd/sdp/model/Course.java
@@ -7,5 +7,35 @@ public class Course
 	private String course_code;
 	private String description;
 	private int credits;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse_code() {
+		return course_code;
+	}
+	public void setCourse_code(String course_code) {
+		this.course_code = course_code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
 	
 }
