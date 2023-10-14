d22a51427b077b2997d16c58c5dce4d2aa352616
Abdul Afroz <abdulafroz59963@gmail.com>
2023-10-14 17:08:43 +0530

    added course.java

--- src/main/java/com/klef/jfsd/sdp/model/Course.java
+++ src/main/java/com/klef/jfsd/sdp/model/Course.java
@@ -2,5 +2,10 @@ package com.klef.jfsd.sdp.model;
 
 public class Course 
 {
-
+	private int id;
+	private String name;
+	private String course_code;
+	private String description;
+	private int credits;
+	
 }
